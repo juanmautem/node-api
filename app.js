0cce83f3e953dc790943722d64acb92f4def5766
juanmagogof1 <iscjuanmafa@gmail.com>
2022-03-08 10:29:34 -0600

    Controladores y Modelos

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-/*Inclusión de las depenencias*/
-var express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
-  mongoose = require("mongoose");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
 
-/* Utilizamos boyParser para  parsear JSON*/
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-/*Utilizamos el método methodOverride para implementar y personalizar métodos HTTP.*/
-app.use(methodOverride());
-
-/*declaración de rutas creando instancia de express*/
-var router = express.Router();
-
-router.get("/", function (req, res) {
-  res.send("Hello World!");
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
 });
 
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
 app.use(router);
 
-app.listen(3000, function () {
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
 });
